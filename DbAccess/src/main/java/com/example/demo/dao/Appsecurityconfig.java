fdc000c3b215231e74bbe7ffae76d7eaaa013972
siddi <siddi@LAPTOP-Q9TRF0K8.Dlink>
2023-01-26 17:36:46 +0530

    spring security form login and Db

--- DbAccess/src/main/java/com/example/demo/dao/Appsecurityconfig.java
+++ DbAccess/src/main/java/com/example/demo/dao/Appsecurityconfig.java
@@ -1,7 +1,10 @@
 package com.example.demo.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,59 +14,122 @@ import org.springframework.security.config.annotation.web.configurers.FormLoginC
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-@Configuration 
+@Configuration
 @EnableWebSecurity
 public class Appsecurityconfig {
- @Bean
- public InMemoryUserDetailsManager userDetailManager() {
-	 System.out.println("user detail manager");
+	@Autowired
+	private MyUserDetailsService userDetailsService;
 
-	 UserDetails user = User.withDefaultPasswordEncoder()
-			 .username("user")
-			 .password("password")
-			 .roles("USER")
-			 .build();
-	 InMemoryUserDetailsManager configurer = new InMemoryUserDetailsManager();
-	 configurer.createUser(user);
-	 return configurer;
-			 
- }
+
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
+	}
+
+/*	@Bean
+	public InMemoryUserDetailsManager userDetailManager() {
+
+		System.out.println("user detail manager");
+
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+				.build();
+		InMemoryUserDetailsManager configurer = new InMemoryUserDetailsManager();
+		configurer.createUser(user);
+		return configurer;
+
+	}*/
+	
+	/*
+	@Bean
+	public UserDetailsManager jpaUserDetailManager() {
+
+		System.out.println("user detail manager");
+
+		
+		UserDetailsManager userDetailsManager = new UserDetailsManager() {
+			
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				return userDetailsService.loadUserByUsername(username);
+			}
+			
+			@Override
+			public boolean userExists(String username) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public void updateUser(UserDetails user) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void deleteUser(String username) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void createUser(UserDetails user) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void changePassword(String oldPassword, String newPassword) {
+				// TODO Auto-generated method stub
+				
+			}
+		};;
+		return userDetailsManager;
+	}
+*/
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-System.out.println("In security configure");
-		return http
-				.csrf(csrf -> csrf.disable())
-				//.authenticationManager(manager -> manager.)
-				.authorizeHttpRequests(auth -> 	
-					auth
-					//
-					.requestMatchers("/getalldepartments").permitAll()
-					//.requestMatchers("/getallstudents").permitAll()
-						.requestMatchers("/user").hasRole("USER")
-						.requestMatchers("/getallstudents").hasRole("USER"))
-						//.requestMatchers("/getalldepartments").hasRole("USER"))
-				//.csrf().disable()
-				//.authorizeHttpRequests()
-				.httpBasic(Customizer.withDefaults())
-				.formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
+		System.out.println("In security configure");
+		return http.csrf(csrf -> csrf.disable())
+				// .authenticationManager(manager -> manager.)
+				.authorizeHttpRequests(auth -> auth
+//					//
+						.requestMatchers("/getalldepartments").permitAll()
+						// .requestMatchers("/getallstudents").permitAll()
+						.requestMatchers("/user").hasRole("USER").requestMatchers("/getallstudents").hasRole("USER"))
+				// .requestMatchers("/getalldepartments").hasRole("USER"))
+				// .csrf().disable()
+				// .authorizeHttpRequests()
+				.httpBasic(Customizer.withDefaults()).formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
 
 					@Override
 					public void customize(FormLoginConfigurer<HttpSecurity> t) {
 						t.defaultSuccessUrl("http://localhost:4200/students");
-						
-					}})
-				//.formLogin(configurer -> confun)
-				//.defaultSuccessUrl("/homepage.html", true)
+
+					}
+				})
+				//.userDetailsService(userDetailsService)
+				.authenticationProvider(authProvider())
+		
+				//.passwordManagement(customizer -> customizer.)
+			// .formLogin(configurer -> confun)
+				// .defaultSuccessUrl("/homepage.html", true)
 
 				.build();
-		
+
 	}
-	
-	//In Angular, write a component - students-list. Create an angular route
-	//In ngOnInit of that component, make an API call to get all students
-	//After login spring boot should redirect to students route
+
+//	//In Angular, write a component - students-list. Create an angular route
+//	//In ngOnInit of that component, make an API call to get all students
+//	//After login spring boot should redirect to students route
+
 }
